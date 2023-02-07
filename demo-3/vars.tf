3f11b17c1b6f11d412171ed22c8e87f1fb52f341
Muhammad Akbar <99253901+akbarmhmmmd@users.noreply.github.com>
2023-02-07 20:01:39 +0700

    update region and ami

--- demo-3/vars.tf
+++ demo-3/vars.tf
@@ -5,7 +5,7 @@ variable "AWS_SECRET_KEY" {
 }
 
 variable "AWS_REGION" {
-  default = "eu-west-1"
+  default = "eu-west-2"
 }
 
 variable "AMIS" {
@@ -14,6 +14,7 @@ variable "AMIS" {
     us-east-1 = "ami-13be557e"
     us-west-2 = "ami-06b94666"
     eu-west-1 = "ami-844e0bf7"
+    eu-west-2 = "ami-09a2a0f7d2db8baca"
   }
 }
 

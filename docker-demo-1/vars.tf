93061c183699ec6e926d5b07039c7bd56ecf16a2
Muhammad Akbar <akbarmhmmmd@gmail.com>
2023-02-07 18:03:04 +0700

    initial commit

---
+++ docker-demo-1/vars.tf
@@ -0,0 +1,17 @@
+variable "AWS_ACCESS_KEY" {
+}
+
+variable "AWS_SECRET_KEY" {
+}
+
+variable "AWS_REGION" {
+  default = "eu-west-1"
+}
+
+variable "AMIS" {
+  type = map(string)
+  default = {
+    us-east-1 = "ami-13be557e"
+    eu-west-1 = "ami-0f29c8402f8cce65c"
+  }
+}
